96d3a1020fd4f531605b338016a83fab68f0d015
REp007 <elamraniy292@gmail.com>
2025-03-16 15:36:54 +0000

    init commit

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from main import add
+
+def test_add():
+    assert add(3, 4) == 7 
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+print("******************")
+test_add()
+print("All tests passed!")
